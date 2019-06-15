948c8a370509cbcf1da8714acac7979cef841213
Viacheslav Zhygulin <s.zhygulin@gmail.com>
2019-06-15 15:11:28 +0300

    removed pki again

--- chaincode/es-exchange/es-chaincode.go
+++ chaincode/es-exchange/es-chaincode.go
@@ -6,7 +6,6 @@ import (
     "github.com/hyperledger/fabric/common/util"
     "github.com/hyperledger/fabric/core/chaincode/shim"
     "github.com/hyperledger/fabric/protos/peer"
-    "github.com/google/easypki/pkg/easypki"
 )
 
 // SimpleAsset implements a simple chaincode to manage an asset
